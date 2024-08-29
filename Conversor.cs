1866658faeeec9521c78877c1a8892d6bed19ac7
Oswaldo Cuellar <ocsaracay@gmail.com>
2024-08-28 20:32:15 -0600

    Propuesta estructura calc imc merge de master a main

---
+++ Conversor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    internal class Conversor
+    {
+        //Clase de utilidad para convertir unidades a metros y kilogramos
+
+    }
+}
---
+++ IMC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC
+{
+    public enum UnidadPeso{ kilogramos, libras }
+    public enum UnidadEstatura { metros, centimetros, pulgadas, pies }
+    internal class IMC
+    {
+        private double estatura;
+        private double peso;
+
+        public double Estatura { get => estatura; set => estatura = value; }
+        public double Peso { get => peso; set => peso = value; }
+
+        public IMC() {
+        }
+
+        public IMC(double estatura, double peso)
+        {
+            Estatura = estatura;
+            Peso = peso;
+            Estatura = estatura;
+            Peso = peso;
+        }
+
+        public double getIMC()
+        {
+            return this.estatura * this.peso;
+        }
+    }
+}
