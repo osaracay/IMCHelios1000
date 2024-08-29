b37120019ecb65c13613bdb48a54c83b131506ea
osaracay <ocsaracay@gmail.com>
2024-08-29 11:02:37 -0600

    Agregue comentario a la clase Conversor

--- Conversor.cs
+++ Conversor.cs
@@ -11,6 +11,11 @@ namespace CalculadoraIMC
 
         //Clase de utilidad para convertir unidades a metros y kilogramos
 
+        /* Se me ocurre para variables: originalValue, conversionFactor, finalValue
+         * Donde originalValue es el valor que quiero convertir
+         * conversionFactor es el coeficiente por el que multiplico el original para obtener el final
+         * Este depende del tipo de conversion a realizar m a ft, kg a lb, y viceversa. 
+         */
 
     }
 }
